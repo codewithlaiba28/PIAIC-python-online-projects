5149c0c9a370c6ce50de084ac8b33383b4adb121
laiba khan <codewithlaiba28@gmail.com>
2025-03-05 00:14:29 +0500

    add

--- Basic/Double_it/app.py
+++ Basic/Double_it/app.py
@@ -1,5 +1,5 @@
 number : int = int(input("Enter a number: "))
 while number < 100:
     number*=2
-    print(str(number, end=" "))
+    print(number, end=" ")
     
--- Basic/liftoff/app.py
+++ Basic/liftoff/app.py
@@ -1,4 +1,16 @@
+import time
 #           start stop step
 for i in range(10, 0, -1):
     print (i)
+    time.sleep(1)
 print("liftoff")        
+
+
+#is ko hum is tarah bhi ker sakte hai
+num = 10
+for i in range(num):
+    print(num)
+    time.sleep(1)
+    num-=1
+print("liftoff")    
+
